2a7302997a2fcf948c579d7611f807c564d824d2
王鑫凯 <772918068@qq.com>
2020-05-14 23:34:44 +0800

    Initial commit

---
+++ user/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import pyzbar.pyzbar as pyzbar
+from PIL import Image, ImageEnhance
+#test code
+img = Image.open("/Users/wangxinkai/Desktop/1.png")
+texts = pyzbar.decode(img)
+for text in texts:
+    tt = text.data.decode("utf-8")
+    print(tt)
+
+# Create your views here.
